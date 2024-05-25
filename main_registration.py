a43ef0334c2c5decb3fbb082cbacd7546aec1012
githubt0p <nikm0311@gmail.com>
2024-05-25 11:23:25 +0400

    create one DB for notes and persons + remake person_id as foreign key

--- main_registration.py
+++ main_registration.py
@@ -2,11 +2,11 @@ from gui.MainWindow import MainWindow as Wn
 import sqlite3
 
 connection = sqlite3.connect('database_all.db')
-cursos = connection.cursor()
+# cursos = connection.cursor()
 #
-# cursos.execute(sql_scripts.person_sql_script_create_table)
+# cursos.execute(person_sql_script_create_table)
 #
-# cursos.execute(sql_scripts.person_sql_script_insert, ("Vasya", "hg5"))
+# cursos.execute(note_sql_script_create_table)
 
 if __name__ == '__main__':
     window = Wn(connection=connection)
